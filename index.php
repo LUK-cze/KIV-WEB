7a9ed1f431cc51943bce26607f666cd46a519329
LUK-cze <45196610+LUK-cze@users.noreply.github.com>
2023-11-05 16:14:08 +0100

    V2
    V2

--- index.php
+++ index.php
@@ -4,32 +4,56 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+
   <link rel="stylesheet" href="css/css.css">
+  <link rel="stylesheet" href="css/navbar.css">
+
+
 
 </head>
 <body>
 
-<div class="hero container-fluid p-5 bg-primary text-white text-center">
-  <h1>U Pardála</h1>
-  <p>Recenze hlavních jídel a nápojů.</p> 
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>                        
+        </button>
+        <a class="navbar-brand glyphicon glyphicon-tower" href="#myPage" alt="logo" width="30" height="30"></a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#about">CO JSME ZAČ?</a></li>
+          <li><a href="#recenze">RECENZE</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<div class="hero jumbotron text-center">
+  <h1>Pár Pařmenů</h1>
+  <p>Recenze video her.</p> 
 </div>
   
 <div class="container mt-5">
   <div class="row">
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+    <div class="col-sm-8">
+      <h3 id="about">Vítej!</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit... Lorem ipsum dolor sit amet, 
+        consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur adipisicing elit 
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
     <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>        
+      <h3>Registruj se!</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
